a0fe3a97f1dcf082837eef187ad0f11eeaa993bd
willnet <netwillnet@gmail.com>
2017-01-12 17:40:29 +0900

    Add clear_ prefix attribute methods

--- mail_form/test/mail_form_test.rb
+++ mail_form/test/mail_form_test.rb
@@ -2,11 +2,16 @@ require 'test_helper'
 require 'fixtures/sample_mail'
 
 class MailForm::Test < ActiveSupport::TestCase
-  test 'sample mail has name and email as attributes' do
+  test 'sample mail can clear attributes using clear_ prefix' do
     sample = SampleMail.new
     sample.name = 'User'
     assert_equal 'User', sample.name
     sample.email = 'user@example.com'
     assert_equal 'user@example.com', sample.email
+
+    sample.clear_name
+    sample.clear_email
+    assert_nil sample.name
+    assert_nil sample.email
   end
 end
