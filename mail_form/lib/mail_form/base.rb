f67d223dbe26b380da7a2962a95a9dc5c07470d2
willnet <netwillnet@gmail.com>
2017-01-19 14:21:40 +0900

    MailForm::Base quirks like ActiveModel

--- mail_form/lib/mail_form/base.rb
+++ mail_form/lib/mail_form/base.rb
@@ -1,11 +1,20 @@
 module MailForm
   class Base
     include ActiveModel::AttributeMethods
+    include ActiveModel::Conversion
+    extend ActiveModel::Naming
+    extend ActiveModel::Translation
+    include ActiveModel::Validations
+
     attribute_method_prefix 'clear_'
 
     def self.attributes(*names)
       attr_accessor(*names)
       define_attribute_methods(names)
+    end
+
+    def persisted?
+      false
     end
 
     protected
