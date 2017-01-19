3040f0fa9b384fe9d9a382e86a115c7a4162785e
willnet <netwillnet@gmail.com>
2017-01-19 14:41:12 +0900

    Add test for delivering mail

--- mail_form/test/mail_form_test.rb
+++ mail_form/test/mail_form_test.rb
@@ -2,6 +2,10 @@ require 'test_helper'
 require 'fixtures/sample_mail'
 
 class MailForm::Test < ActiveSupport::TestCase
+  setup do
+    ActionMailer::Base.deliveries.clear
+  end
+
   test 'sample mail can clear attributes using clear_ prefix' do
     sample = SampleMail.new
     sample.name = 'User'
@@ -13,5 +17,15 @@ class MailForm::Test < ActiveSupport::TestCase
     sample.clear_email
     assert_nil sample.name
     assert_nil sample.email
+  end
+
+  test 'delivers an email with attributes' do
+    sample = SampleMail.new
+    sample.email = 'user@example.com'
+    sample.deliver
+    assert_equal 1, ActionMailer::Base.deliveries.size
+    mail = ActionMailer::Base.deliveries.last
+    assert_equal ['user@example.com'], mail.from
+    assert_match 'Email: user@example.com', mail.body.encoded
   end
 end
